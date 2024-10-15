06114a9d9ce1c5a14e44c741d35f7b9822ece6a0
Petar Markovic <petarmarkovic@Petars-MBP.lan>
2024-10-15 09:47:25 +0200

    chore: fix env vars

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3000);
+  const port = configService.get<number>('PORT', 3200);
   const client = configService.get<string>(
     'APP_CLIENT',
     'http://localhost:5300',
