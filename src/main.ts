ca5c786e77011122eca2967bf8d81f9ad0b3cae2
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2025-01-30 15:01:58 +0100

    chore: remove vercel a

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
 import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
